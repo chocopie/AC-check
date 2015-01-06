f0146e277e602985926ccb3b1bbc428780f26145
Karl Groves <karl@karlgroves.com>
2015-01-06 12:02:08 -0500

    Mod to URL in background.js

--- background.js
+++ background.js
@@ -1,4 +1,4 @@
-var urlToVisit = "http://www.tenon.io/#testnow";
+var urlToVisit = "http://tenon.io/testNow.php";
 chrome.browserAction.onClicked.addListener(function (tab) {
     var current = tab.url;
     chrome.tabs.create({"url": urlToVisit + '?url=' + encodeURIComponent(current)});
